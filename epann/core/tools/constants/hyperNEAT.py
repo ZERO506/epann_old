187c8395f0fcd5089f857da8cf32c1caa0bfdb3f
Chad Carlson <chadwcarlson@users.noreply.github.com>
2017-04-13 11:01:18 -0400

    First commit

---
+++ epann/core/tools/constants/hyperNEAT.py
@@ -0,0 +1,18 @@
+
+
+c_1, c_2, c_3, C_t = 1.0, 1.0, 3.0, 4.0
+
+cx_percentage = 0.60
+
+prob_add_node = 0.03
+prob_add_connection = 0.3
+
+prob_perturb_weight = 0.25
+prob_replace_weight = 0.5
+
+prob_delete_connection = 0
+prob_flip_enable_bit = 0
+
+allow_self_connections = False
+
+connectionwise_mutations = False
---
+++ example.py
@@ -0,0 +1,26 @@
+
+
+from epann.experiments.experiment import Experiment
+
+
+num_generations, num_agents = 30, 36
+
+exp = Experiment( num_generations, num_agents, verbose=True)
+
+exp.run()
+
+
+
+#
+gen, agent = 0, 26
+
+print exp.history[gen].genomes[agent].connections
+print exp.history[gen].genomes[agent].nodes
+# print [ exp.history[gen].genomes[agent].connections[connection]['weight'] for connection in exp.history[gen].genomes[agent].connections.keys()]
+
+gen, agent = num_generations-1, 26
+
+print '\n', exp.history[gen].genomes[agent].connections
+print exp.history[gen].genomes[agent].nodes
+
+# print [ exp.history[gen].genomes[agent].connections[connection]['weight'] for connection in exp.history[gen].genomes[agent].connections.keys()]
